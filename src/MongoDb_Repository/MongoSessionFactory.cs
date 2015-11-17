8b988e6c218280839192a0557a5b55c84b682db2
Matteo Selleri <matteo.selleri@outlook.it>
2015-11-17 17:01:36 +0100

    Update MongoSessionFactory.cs

--- src/MongoDb_Repository/MongoSessionFactory.cs
+++ src/MongoDb_Repository/MongoSessionFactory.cs
@@ -12,21 +12,6 @@ namespace MongoDb_Repository
     {
         public static MongoDatabase GetMongoDatabase()
         {
-            //const string uri = "mongodb://localhost:27017/";
-            //var client = new MongoClient(uri);
-            //var db = client.GetDatabase(new MongoUrl(uri).DatabaseName);
-            //var collection = db.GetCollection<Book>("Books");
-            //return client.GetServer().GetDatabase("BookMVC");
-
-
-
-
-            //var connectionString = "mongodb://localhost:27017/";
-            //var client = new MongoClient(connectionString).GetServer();
-            //var db = client.GetDatabase("BookMVC");
-            //var collection = db.GetCollection<Book>("Books");
-            //return client.GetDatabase("BookMVC");
-
             string connectionString = "mongodb://localhost:27017/";
             var mongoClientSettings = new MongoClientSettings();
             mongoClientSettings.Server = new MongoServerAddress(connectionString);
